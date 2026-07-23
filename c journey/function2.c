9fe15d6978e3c8ca5ca28783beb1fb8a546ac906
Abhinav Maurya <abhinavmaurya7740@gmail.com>
2026-07-23 21:55:37 +0530

    nothing

--- c journey/function2.c
+++ c journey/function2.c
@@ -1,4 +1,5 @@
-/* Takes something Return nothing */
+/* Takes something Return nothing  */
+/*Here we must use void because it is Takes something Return nothing type if we use int instead of void it will may misbehave */
 #include <stdio.h>
 void add(int a,int b );
 int main()
