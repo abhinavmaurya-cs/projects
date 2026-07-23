a9a035a3f70b2b3b895dce09788c4daca57bf61f
Abhinav Maurya <abhinavmaurya7740@gmail.com>
2026-07-23 13:53:05 +0530

    enchanced

--- c journey/function.c
+++ c journey/function.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
-int main()
+int a();
+int b();
+int main(){
+
+    a();
+    a();
+      b();
+return 0;
+
+ }
+int a()
 {
-
+    printf("\nI am very happy");
 }
-a()
-{}
-
-
-
-
-
+int b()
+{
+    printf("\nyou are very bad as you do nothing at your home");
+}
